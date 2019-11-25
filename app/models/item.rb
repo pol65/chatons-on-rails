21b0d31fc117248329999bf51c7c53161acd8289
Polo <paul.poitrenaud@gmail.com>
2019-11-25 18:15:13 +0100

    validations added to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+    validates :title, :description, :price, :image_url, presence: true
+    validates :title, length: { in: 5..90 }
+    validates :description, length: { in: 20..500 }
+    validates :price, numericality: { greater_than: 0 }
 end
