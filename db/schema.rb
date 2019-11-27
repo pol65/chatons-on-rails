0aa727b577ad2785c8ebc739ede3e39da0313017
Cano <barbara.cano.gaso@gmail.com>
2019-11-27 17:25:02 +0100

    syntax

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,10 @@ ActiveRecord::Schema.define(version: 2019_11_27_134119) do
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_carts_on_item_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
