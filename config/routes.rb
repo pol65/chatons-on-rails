4708a742a917e127d0dc6cb6ab5acac71a7e7fa5
Polo <paul.poitrenaud@gmail.com>
2019-11-27 10:30:04 +0100

    dev working, pushing on master

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
-   get'static_pages/secret'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
