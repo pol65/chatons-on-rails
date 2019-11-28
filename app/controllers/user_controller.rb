c27ea796d3b6c714e6c7885961a3e12d7af39c38
Cano <barbara.cano.gaso@gmail.com>
2019-11-28 16:47:01 +0100

    button and route okay

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,8 @@
 class UserController < ApplicationController
+    before_action :authenticate_user!, only: [:show]
     def index
     end
     def show
+        @user = User.find(current_user.id)
     end
 end
