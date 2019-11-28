914f7cb1c0f2df43e9dfbcee88f4fed1f570f3d6
Cano <barbara.cano.gaso@gmail.com>
2019-11-28 16:54:05 +0100

    profil picture

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,6 @@
 class UserController < ApplicationController
     before_action :authenticate_user!, only: [:show]
-    def index
-    end
+    
     def show
         @user = User.find(current_user.id)
     end
